658afd96af782b252c1e4927e2a546612e28efa7
mand2 <nayeon.kim1123@gmail.com>
2020-09-27 00:00:33 +0900

    chore: gradle 버전 변경-4.10.2

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created By mand2 on 2020-09-26.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
